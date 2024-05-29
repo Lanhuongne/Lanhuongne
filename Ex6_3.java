13c4884725fbdb66dda5f662dfc1b69240bdd1a9
Lanhuongne <166737268+Lanhuongne@users.noreply.github.com>
2024-05-29 12:30:32 +0700

    Add files via upload

---
+++ Ex6_3.java
@@ -0,0 +1,18 @@
+package Ex6;
+
+public class Ex6_3 {
+	public static void main(String[] args) {
+		for (int i=1;i<10;i++) 
+	
+		   {for (int j=1;j<10;j++) 
+		   { int x=j*i;
+			if (j%9==0) 
+			{System.out.print("\n");}
+			else 
+			{System.out.print(x+"\t");}
+			}
+		   }
+		}
+	}
+
+
